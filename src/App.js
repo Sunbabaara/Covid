069a4a41452c0d7af4f1263dce066dd9296e2e32
Sunbabaara <csunbabaara@gmail.com>
2021-03-21 22:59:40 +0000

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import Header from "./components/Header";
+import './App.css';
+import Stats from "./components/Stats";
+import CountrySelector from "./components/CountrySelector";
+
+function App() { 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <h1>Global summary</h1>
+      <Stats url="https://covid19.mathdro.id/api"/>
+      <CountrySelector/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+        <header>
+        <h1>COVID STATISTICAL APP</h1>
+             </header>
+    )
+}
+export default Header;
